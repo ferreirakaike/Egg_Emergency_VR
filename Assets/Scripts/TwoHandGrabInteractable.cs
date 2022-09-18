e2f17b1b49b865af280b9d2dcd839451351506e7
Dat Nguyen <dat.npt@gmail.com>
2022-09-18 16:31:58 -0400

    Add primitve 2 hands grab script

---
+++ Assets/Scripts/TwoHandGrabInteractable.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TwoHandGrabInteractable : XRGrabInteractable
+{
+    public List<XRSimpleInteractable> secondHandGrabPoints = new List<XRSimpleInteractable>();
+    private XRBaseInteractor secondInteractor;
+    private Quaternion attachInitialRotation;
+    public enum TwoHandRotationType { None, First, Second};
+    public TwoHandRotationType twoHandRotationType;
+    public bool snapToSecondHand = true;
+    private Quaternion initialRotationOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add listener all second hand grab points
+        foreach (var item in secondHandGrabPoints)
+        {
+            item.onSelectEnter.AddListener(OnSecondHandGrab);
+            item.onSelectExit.AddListener(OnSecondHandRelease);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        if(secondInteractor && selectingInteractor)
+        {
+            // Compute rotation
+            if(snapToSecondHand)
+            {
+                selectingInteractor.attachTransform.rotation = GetTwoHandRotation();
+            }
+            else
+            {
+                selectingInteractor.attachTransform.rotation = GetTwoHandRotation() * initialRotationOffset;
+            }
+        }
+        base.ProcessInteractable(updatePhase);
+    }
+
+    private Quaternion GetTwoHandRotation()
+    {
+        Quaternion targetRotation;
+        if (twoHandRotationType == TwoHandRotationType.None)
+        {
+            targetRotation = Quaternion.LookRotation(secondInteractor.attachTransform.position - selectingInteractor.attachTransform.position);
+        }
+        else if (twoHandRotationType == TwoHandRotationType.First)
+        {
+            targetRotation = Quaternion.LookRotation(secondInteractor.attachTransform.position - selectingInteractor.attachTransform.position, selectingInteractor.attachTransform.up);
+        }
+        else
+        {
+            targetRotation = Quaternion.LookRotation(secondInteractor.attachTransform.position - selectingInteractor.attachTransform.position, secondInteractor.attachTransform.up);
+        }
+        return targetRotation;
+    }
+
+    public void OnSecondHandGrab(XRBaseInteractor interactor)
+    {
+        Debug.Log("Second hand grab");
+        secondInteractor = interactor;
+        initialRotationOffset = Quaternion.Inverse(GetTwoHandRotation()) * selectingInteractor.attachTransform.rotation;
+    }
+
+    public void OnSecondHandRelease(XRBaseInteractor interactor)
+    {
+        Debug.Log("Second hand release");
+        secondInteractor = null;
+    }
+
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        Debug.Log("First hand grab");
+        base.OnSelectEnter(interactor);
+        attachInitialRotation = interactor.attachTransform.localRotation;
+    }
+
+    protected override void OnSelectExit(XRBaseInteractor interactor)
+    {
+        Debug.Log("First hand release");
+        base.OnSelectExit(interactor);
+        secondInteractor = null;
+        interactor.attachTransform.localRotation = attachInitialRotation;
+    }
+
+    public override bool IsSelectableBy(XRBaseInteractor interactor)
+    {
+        bool is_already_grabbed = selectingInteractor && !interactor.Equals(selectingInteractor);
+        return base.IsSelectableBy(interactor) && !is_already_grabbed;
+    }
+}
