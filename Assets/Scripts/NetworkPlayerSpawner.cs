956cffb59ddf8a366a17a4cd28e3c9cd3d0a3e16
Dat Nguyen <dat.npt@gmail.com>
2022-11-06 21:27:24 -0500

    WorkInProgress: commit to save current work offsite

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -18,18 +18,6 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
     void Start()
     {
         networkVar = GameObject.Find("Network Interaction Statuses").GetComponent<NetworkVariablesAndReferences>();
-    }
-
-    void Update()
-    {
-        if (networkVar.isGameOver)
-        {
-            spawnedPlayerPrefab.SetActive(false);
-        }
-    }
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
         if (PhotonNetwork.IsMasterClient)
         {
             spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", playerSpawnLocations[0].position, playerSpawnLocations[0].rotation);
@@ -55,6 +43,42 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         }
         Debug.Log("Joined Room");
     }
+
+    void Update()
+    {
+        if (networkVar.isGameOver)
+        {
+            spawnedPlayerPrefab.SetActive(false);
+        }
+    }
+    // public override void OnJoinedRoom()
+    // {
+    //     base.OnJoinedRoom();
+    //     if (PhotonNetwork.IsMasterClient)
+    //     {
+    //         spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", playerSpawnLocations[0].position, playerSpawnLocations[0].rotation);
+    //         spawnedBasketPrefab = PhotonNetwork.Instantiate("Network Basket", basketSpawnLocations[0].position, basketSpawnLocations[0].rotation);
+    //         spawnedShadowBasketPrefab = PhotonNetwork.Instantiate("Network Shadow Basket", basketSpawnLocations[0].position, basketSpawnLocations[0].rotation);
+    //         spawnedBasketPrefab.transform.localScale = new Vector3(25,25,25);
+    //         networkVar.UpdateBasketIDs(spawnedBasketPrefab.GetPhotonView().ViewID, 0);
+    //         networkVar.UpdateShadowBasketIDs(spawnedShadowBasketPrefab.GetPhotonView().ViewID, 0);
+    //         networkVar.UpdatePlayerIDs(spawnedPlayerPrefab.GetPhotonView().ViewID, 0);
+    //     }
+    //     else
+    //     {
+    //         spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", playerSpawnLocations[1].position, playerSpawnLocations[1].rotation);
+    //         XROrigin origin = FindObjectOfType<XROrigin>();
+    //         origin.transform.position = playerSpawnLocations[1].position;
+    //         origin.transform.rotation = playerSpawnLocations[1].rotation;
+    //         spawnedBasketPrefab =PhotonNetwork.Instantiate("Network Basket", basketSpawnLocations[1].position, basketSpawnLocations[1].rotation);
+    //         spawnedShadowBasketPrefab = PhotonNetwork.Instantiate("Network Shadow Basket", basketSpawnLocations[1].position, basketSpawnLocations[1].rotation);
+    //         spawnedBasketPrefab.transform.localScale = new Vector3(25,25,25);
+    //         networkVar.UpdateBasketIDs(spawnedBasketPrefab.GetPhotonView().ViewID, 1);
+    //         networkVar.UpdateShadowBasketIDs(spawnedShadowBasketPrefab.GetPhotonView().ViewID, 0);
+    //         networkVar.UpdatePlayerIDs(spawnedPlayerPrefab.GetPhotonView().ViewID, 1);
+    //     }
+    //     Debug.Log("Joined Room");
+    // }
 
     public override void OnLeftRoom()
     {
