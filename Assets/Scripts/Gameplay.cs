9bdf6c981f560e759e2f09a9b6dc5dada2a013af
Dat Nguyen <dat.npt@gmail.com>
2022-11-12 19:45:28 -0500

    Prepare script to send deterrent

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -72,63 +72,99 @@ public class Gameplay : MonoBehaviourPunCallbacks
             yield return new WaitForSeconds(spawnTime);
             int deterrentRoll = Random.Range(0, 100);
             int chosenPath = Random.Range(0, 3);
-            photonView.RPC("spawnCollectable", RpcTarget.All, deterrentRoll, chosenPath, difficulty);
+            photonView.RPC("spawnCollectable", RpcTarget.All, deterrentRoll, chosenPath, difficulty, -1);
         }
     }
 
-    [PunRPC]
-    private void spawnCollectable(int deterrentRoll, int chosenPath, float synced_difficulty) {
-		if (GameplayManager.gameIsOver) {
-			return;
-		}
-		
-        if (deterrentRoll < (int)deterrentChance) {
-            a = PhotonNetwork.Instantiate("Deterrent_Bomb", transform.position, new Quaternion(-90,0,0,0)) as GameObject;
-            a.tag = "Deterrent";
-        }
-        else {
-            a = PhotonNetwork.Instantiate("Collectable", transform.position, new Quaternion(-90,0,0,0)) as GameObject;
-            a.tag = "Collectable";
-        }
-        // Since object is spawned using PhotonNetwork.Instantiate, let photon handle viewID assignment
-        
-        var script = a.GetComponent<PathFollower>();
-        script.speed = synced_difficulty;
-        var script2 = a.GetComponent<CollectableBehavior>();
-        
-        script.endOfPathInstruction = end;
-        
-        // master uses path left, path, right.
-        // client uses path left2, path2, right2
+    /// <summary>
+    /// Method for sending deterrent toward the other player
+    /// </summary>
+    public void sendDeterrent()
+    {
+        // maybe change deterrent mat a bit to differentiate
+        // change mat in spawnCollectable
+        int target_player;
         if (PhotonNetwork.IsMasterClient)
         {
-            // set this to 0 or 1for multiplayer
-            script2.playerIndex = 0;
-            if (chosenPath == 0) {
-                script.pathCreator = leftPath;
-            }
-            else if (chosenPath == 1) {
-                script.pathCreator = path;
-            }
-            else if (chosenPath == 2) {
-                script.pathCreator = rightPath;
-            }
+            target_player = 1;
         }
         else
         {
-            // set this to 0 or 1for multiplayer
-            script2.playerIndex = 1;
-            if (chosenPath == 0) {
-                script.pathCreator = leftPath2;
-            }
-            else if (chosenPath == 1) {
-                script.pathCreator = path2;
-            }
-            else if (chosenPath == 2) {
-                script.pathCreator = rightPath2;
-            }
+            target_player = 0;
         }
-        a.SetActive(true);
+        int chosenPath = Random.Range(0, 3);
+        photonView.RPC("spawnCollectable", RpcTarget.All, 100, chosenPath, difficulty, target_player);
+    }
+
+    [PunRPC]
+    private void spawnCollectable(int deterrentRoll, int chosenPath, float synced_difficulty, int target_player = -1) {
+		if (GameplayManager.gameIsOver) {
+			return;
+		}
+
+        // update difficulty for client
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            difficulty = synced_difficulty;
+        }
+		if (target_player == -1 || (target_player == 0 && PhotonNetwork.IsMasterClient) || (target_player == 1 && !PhotonNetwork.IsMasterClient))
+        {
+            if (deterrentRoll < (int)deterrentChance) {
+                a = PhotonNetwork.Instantiate("Deterrent_Bomb", transform.position, new Quaternion(-90,0,0,0)) as GameObject;
+                a.tag = "Deterrent";
+            }
+            else {
+                a = PhotonNetwork.Instantiate("Collectable", transform.position, new Quaternion(-90,0,0,0)) as GameObject;
+                a.tag = "Collectable";
+            }
+            // Since object is spawned using PhotonNetwork.Instantiate, let photon handle viewID assignment
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Change mat when player send deterrent intentially
+            if (target_player != -1)
+            {
+                // TODO:
+            }
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            var script = a.GetComponent<PathFollower>();
+            script.speed = synced_difficulty;
+            var script2 = a.GetComponent<CollectableBehavior>();
+            
+            script.endOfPathInstruction = end;
+            
+            // master uses path left, path, right.
+            // client uses path left2, path2, right2
+            if (PhotonNetwork.IsMasterClient)
+            {
+                // set this to 0 or 1for multiplayer
+                script2.playerIndex = 0;
+                if (chosenPath == 0) {
+                    script.pathCreator = leftPath;
+                }
+                else if (chosenPath == 1) {
+                    script.pathCreator = path;
+                }
+                else if (chosenPath == 2) {
+                    script.pathCreator = rightPath;
+                }
+            }
+            else
+            {
+                // set this to 0 or 1for multiplayer
+                script2.playerIndex = 1;
+                if (chosenPath == 0) {
+                    script.pathCreator = leftPath2;
+                }
+                else if (chosenPath == 1) {
+                    script.pathCreator = path2;
+                }
+                else if (chosenPath == 2) {
+                    script.pathCreator = rightPath2;
+                }
+            }
+            a.SetActive(true);
+        }
     }
 
 }
